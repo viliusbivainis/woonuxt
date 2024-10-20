63121ba05ee6624156cd1c999dde742e0ea03da3
ViliusBivainis <viliusbiv@gmail.com>
2024-10-20 22:26:37 +0300

    new logo

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -3,8 +3,7 @@ const { wishlistLink } = useAuth();
 </script>
 
 <template>
-  <nav>
-    <NuxtLink to="/">{{ $t('messages.general.home') }}</NuxtLink>
+  <nav class="main-nav">
     <NuxtLink to="/products">{{ $t('messages.general.allProducts') }}</NuxtLink>
     <NuxtLink to="/categories">{{ $t('messages.shop.category', 2) }}</NuxtLink>
     <NuxtLink to="/contact">{{ $t('messages.general.contact') }}</NuxtLink>
@@ -12,3 +11,16 @@ const { wishlistLink } = useAuth();
     <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
   </nav>
 </template>
+
+<style scoped>
+.main-nav {
+  font-family: "FixelText-SemiBold", sans-serif;
+  font-size: 16px;
+  line-height:24px;
+  font-weight:600;
+  color:#212121;
+  vertical-align: baseline;
+  outline: 0;
+  background-color: transparent;
+}
+</style>
